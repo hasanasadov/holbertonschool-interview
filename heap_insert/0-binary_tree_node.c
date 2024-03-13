8e70e359c7e06b0678f4894262e73d4ff0a8e496
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-03-13 17:14:01 +0400

    Update 0-binary_tree_node.c

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent of tree
+ * @value: value of new node
+ * Return: new node or NULL if failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
