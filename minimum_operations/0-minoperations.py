5ed96eceb9008b7e23b828ff0c3db9cac43bd9dc
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-03-28 11:36:57 +0400

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""function"""
+
+
+"""
+Calculates the minimum operations
+"""
 
 
 def minOperations(n):
-    """Args:
-            n: repetitions of H
-
-        Returns:
-            number of operations (Copy & Paste) to reach n Hs
-    """
-    operations = 0
-    summation = 1
-    carrier = 0
-
-    while summation < n:
-        if n % summation == 0:
-            carrier = summation
-            summation *= 2
-            operations += 1
-        else:
-            summation += carrier
-        operations += 1  # Always paste
-
-    return operations
+    answer = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            answer += x
+            n /= x
+        x += 1
+    return answer
