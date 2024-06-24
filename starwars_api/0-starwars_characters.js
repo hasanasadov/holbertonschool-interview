7c482036e3ab8b191da85cb95c80dd61653e59f1
hasanasadov <125257868+hasanasadov@users.noreply.github.com>
2024-06-24 20:23:55 +0400

    Update 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,26 +1,33 @@
+
 #!/usr/bin/node
-
-
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
-const arg = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
 
-const charNames = (characters, i = 0) => {
-  if (i === characters.length) return;
-  request(characters[i], (error, response, body) => {
-    if (error) throw error;
-    // Convert a string of characters JSON to a JavaScript object and print it
-    console.log(JSON.parse(body).name);
-    // Call recursively the function charNames and increment it to pass to the next character
-    charNames(characters, i + 1);
+function retrive (urlChar) {
+  return new Promise(function (resolve, reject) {
+    request(urlChar, function getChar (err2, response2, body2) {
+      if (err2) {
+        reject(err2);
+      } else {
+        resolve(JSON.parse(body2).name);
+      }
+    });
   });
-};
+}
 
-// Request to the API to recover movie information
-request(url, function (error, response, body) {
-  if (error) throw error;
-  // Extract URL of characters from the body JSON response and store it in a var char
-  const char = JSON.parse(body).characters;
-  // Call the function charNames to print the characters
-  charNames(char);
+async function getlist (urlist) {
+  for (const urlChar of urlist) {
+    const character = await retrive(urlChar);
+    console.log(character);
+  }
+}
+
+request(url, function getList (err, response, body) {
+  if (err) {
+    throw err;
+  } else {
+    const urlist = JSON.parse(body).characters;
+    getlist(urlist);
+  }
 });
