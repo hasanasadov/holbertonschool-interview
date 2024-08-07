d66a14cf97206f07e5ec5da18d858e3ab712e057
Hasanali Asadov <125257868+hasanasadov@users.noreply.github.com>
2024-08-07 12:10:53 +0400

    Update 0-rotate_2d_matrix.py

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """
-Module used to rotate a matrix 90 degrees
+Rotate 2D Matrix
 """
 
 
 def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
     """
-    Given an n x n 2D matrix, rotate it 90 degrees.
-    Do not return anything. The matrix must be edited in-place.
-    You can assume the matrix will have 2 dim and will not be empty.
-
-    Arguments
-    ---------
-    - matrix  : matrix
-                list of non-negative integers.
-
-    Returns
-    -------
-    - Nothing. The function modifies the original argument
-    """
-
-    # create a replica of the original matrix
-    copy = matrix[:]
-
-    for i in range(len(matrix)):
-
-        # extract the i column from the copy
-        col_i = [row[i] for row in copy]
-
-        # place it on the original matrix in reverse order
-        matrix[i] = col_i[::-1]
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
